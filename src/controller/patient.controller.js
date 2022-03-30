27c9fa4cf403315c615502a00e9fa510f9b4a706
Rishit Pandey <rishpandey8097@gmail.com>
2022-03-30 18:08:03 +0530

    Add: more controller functions

--- src/controller/patient.controller.js
+++ src/controller/patient.controller.js
@@ -131,6 +131,82 @@ const getDriver = (req, res) => {
   });
 };
 
+export const updatePatient = (req, res) => {
+  logger.info(`${req.method} ${req.originalUrl}, fetching patient`);
+  database.query(QUERY.SELECT_PATIENT, [req.params.id], (error, results) => {
+    if (!results[0]) {
+      res
+        .status(HttpStatus.NOT_FOUND.code)
+        .send(
+          new Response(
+            HttpStatus.NOT_FOUND.code,
+            HttpStatus.NOT_FOUND.status,
+            `Patient by id ${req.params.id} was not found`
+          )
+        );
+    } else {
+      logger.info(`${req.method} ${req.originalUrl}, updating patient`);
+      database.query(
+        QUERY.UPDATE_PATIENT,
+        [...Object.values(req.body), req.params.id],
+        (error, results) => {
+          if (!error) {
+            res
+              .status(HttpStatus.OK.code)
+              .send(
+                new Response(
+                  HttpStatus.OK.code,
+                  HttpStatus.OK.status,
+                  `Patient updated`,
+                  { id: req.params.id, ...req.body }
+                )
+              );
+          } else {
+            logger.error(error.message);
+            res
+              .status(HttpStatus.INTERNAL_SERVER_ERROR.code)
+              .send(
+                new Response(
+                  HttpStatus.INTERNAL_SERVER_ERROR.code,
+                  HttpStatus.INTERNAL_SERVER_ERROR.status,
+                  `Error occurred`
+                )
+              );
+          }
+        }
+      );
+    }
+  });
+};
+
+export const deletePatient = (req, res) => {
+  logger.info(`${req.method} ${req.originalUrl}, deleting patient`);
+  database.query(QUERY.DELETE_PATIENT, [req.params.id], (error, results) => {
+    if (results.affectedRows > 0) {
+      res
+        .status(HttpStatus.OK.code)
+        .send(
+          new Response(
+            HttpStatus.OK.code,
+            HttpStatus.OK.status,
+            `Patient deleted`,
+            results[0]
+          )
+        );
+    } else {
+      res
+        .status(HttpStatus.NOT_FOUND.code)
+        .send(
+          new Response(
+            HttpStatus.NOT_FOUND.code,
+            HttpStatus.NOT_FOUND.status,
+            `Patient by id ${req.params.id} was not found`
+          )
+        );
+    }
+  });
+};
+
 module.exports = {
   HttpStatus,
   getDrivers,
